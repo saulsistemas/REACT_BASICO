e30190d7a108746374d1421d4929c4cda28dba42
saul <saul@.hotmail.com>
2022-04-07 17:46:17 -0500

    variables

---
+++ 02-intro-javascript/src/BASES/01-const-let.js
@@ -0,0 +1,17 @@
+//VARIABLES
+
+//LET SOLO FUNCIONA EN EL CONTEXTO
+//CONST NUNCA CAMBIO
+const nombre = "saul";
+const apellido ="Santamaria";
+
+let valorDado = 5;
+
+ valorDado=6;
+
+console.log(nombre, apellido,valorDado);
+
+if (true) {
+    let valorDado=7;
+    console.log(valorDado);
+}
