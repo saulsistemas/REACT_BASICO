15419b366fc16ec291c3651612faef7ed0dd9ae8
saul <saul@.hotmail.com>
2022-04-09 08:52:10 -0500

    arreglo

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,14 @@
+const personajes = ['goku','vegeta','trunks'];
+//console.log(personajes[0]);
+//console.log(personajes[1]);
+//console.log(personajes[2]);
+
+//IGNORE AL PRIMERO
+const [,p2] = personajes;
+console.log(p2);
+
+const retornaArreglo = ()=>{
+    return ['abc',12132]
+} 
+
+console.log(retornaArreglo());
