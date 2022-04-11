a1b73052ade89dbb8de993f76f9460d3851514f4
saul <saul@.hotmail.com>
2022-04-11 11:59:45 -0500

    fragment, mandar varios elementos html

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,7 +1,15 @@
 //FC
 
+//<> es un fragment
+
 const PrimeraApp = ()=>{
-    return <h1>Hola mundo</h1>;
+    return (
+        <> 
+            <h1>Hola mundo</h1>
+            <p>Hola mundo</p>
+        </>
+        );
+
 }
 
 export default PrimeraApp;
