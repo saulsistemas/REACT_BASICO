0f7cf0305a64295917d9f748922bf20ca7ce01a2
saul <saul@.hotmail.com>
2022-04-11 11:51:26 -0500

    function component

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,7 @@
+//FC
+
+const PrimeraApp = ()=>{
+    return <h1>Hola mundo</h1>;
+}
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+
+import './index.css'
 
 
-const saludo = <h1>Hola mundo</h1>;
-console.log(saludo);
+
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(saludo, divRoot)
+ReactDOM.render(<PrimeraApp />, divRoot)
 
 //console.log(divRoot);
