61ac1b54e5319c8350940f232f8930830cca377d
saul <saul@.hotmail.com>
2022-04-11 12:30:04 -0500

    Deafault Props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,13 +2,14 @@
 import PropTypes from 'prop-types';
 //<> es un fragment
 //DESTRUCTURACION
-const PrimeraApp = ( {saludo })=>{
+const PrimeraApp = ( {saludo, subtitulo  })=>{
   
     console.log(saludo);
     return (
         <> 
             <h1>Hola mundo </h1>
             <h2>{saludo}</h2>
+            <h2>{subtitulo}</h2>
             <p>Hola mundo</p>
         </>
         );
@@ -17,5 +18,8 @@ const PrimeraApp = ( {saludo })=>{
 }
 PrimeraApp.propTypes={
             saludo:PropTypes.number.isRequired
+}
+PrimeraApp.defaultProps ={
+            subtitulo:'spy un subtitulo',
 }
 export default PrimeraApp;
