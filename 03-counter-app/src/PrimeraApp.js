283473d413c3c545c9c9471d649a2ffc221050e8
saul <saul@.hotmail.com>
2022-04-11 12:06:12 -0500

    variables en el html

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -3,9 +3,12 @@
 //<> es un fragment
 
 const PrimeraApp = ()=>{
+
+    const saludo = "Hola mundo desde variable";
     return (
         <> 
-            <h1>Hola mundo</h1>
+            <h1>Hola mundo </h1>
+            <h2>{saludo}</h2>
             <p>Hola mundo</p>
         </>
         );
