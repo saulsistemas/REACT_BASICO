6482d4dd16aece67ec37f433069f695e36bb237d
saul <saul@.hotmail.com>
2022-04-11 14:09:33 -0500

    c couter

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 //INGRESAN COMO OBJETO LUEGO SE RESTRUCTURA {}
 
 const CounterApp = ({value})=>{
+    //DESESTRUCTURACION
+    const [counter,setCounter] = useState(0)
+    //(2) ['Goku', ƒ]
+    console.log(counter);
+
     //handleAdd
-    const handleadd =(e)=>{
-        console.log(e)
+    const handleadd =()=>{
+        //setCounter(counter+1)
+        setCounter((c)=>c+1)
     }
     return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
             
-        <button onClick={handleadd()}>+1 Enviar</button>
+        <button onClick={handleadd}>+1 Enviar</button>
     </>
     );
 }
